6c320eb9bb1b6986fac9e035dc9ed751a474f039
Douglas Pallone <douglaspallone@gmail.com>
2026-01-28 21:46:52 -0300

    Atualização CNAME

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Isso permite que o GitHub Pages leia seu site
+  output: 'export', // ADICIONE ESTA LINHA
   typescript: {
     ignoreBuildErrors: true,
   },
